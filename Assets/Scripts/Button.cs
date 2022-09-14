6a7b458640e18e95afa82126f130e375399edc78
UniCoti <kimjjhh3103@gmail.com>
2022-09-15 01:14:52 +0900

    세번째 커밋
    몇번 클릭했는지 횟수 보여주는 기능 추가
    최적화(필요없는 코드 삭제)
    초기화 버튼도 횟수 초기화 코드 추가

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using TMPro;
 
 public class Button : MonoBehaviour
 {
@@ -9,7 +10,8 @@ public class Button : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log(GM.Countext.text.Substring(0, GM.Countext.text.Length - 3 -(GM.count.ToString().Length)));
+        Debug.Log(GM.Countext.text.Substring(0, GM.Countext.text.Length - 4).Length);
     }
 
     // Update is called once per frame
@@ -21,9 +23,18 @@ public class Button : MonoBehaviour
     public void Btn() { //공용 버튼
         var cost = EventSystem.current.currentSelectedGameObject.GetComponent<prize>(); //누른 버튼의 prize컴포넌트 가져오기
         GM.AddPrize(cost.prizes[cost.index]);
+        if(GM.Countext.text.Substring(0, GM.Countext.text.Length - 3 -(GM.count.ToString().Length)) == EventSystem.current.currentSelectedGameObject.GetComponentInChildren<TextMeshProUGUI>().text) { //같은 항목이라면
+            GM.count++; //갯수 추가
+            GM.Countext.text = EventSystem.current.currentSelectedGameObject.GetComponentInChildren<TextMeshProUGUI>().text + " : " + GM.count; //터치 개수 표시
+        } else { //다른 항목이라면
+            GM.count = 1; //개수 초기화
+            GM.Countext.text = EventSystem.current.currentSelectedGameObject.GetComponentInChildren<TextMeshProUGUI>().text + " : " + GM.count; //터치 개수 표시
+        }
     }
 
     public void AC() { //초기화
         GM.cost = 0;
+        GM.count = 0;
+        GM.Countext.text = "터치 횟수 : 0";
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,7 +7,9 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Gm;
     public TextMeshProUGUI TotalText;
+    public TextMeshProUGUI Countext;
     public float cost;
+    public float count;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,11 +19,12 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        TotalText.text = cost.ToString() + "원";
+        //Countext.text = "터치 횟수 : " + count.ToString();
+        TotalText.text = cost.ToString() + "원"; // 가격 텍스트에 표시
     }
 
     public void AddPrize(float add) {
         cost += add; //가격 추가
-        TotalText.text = cost.ToString() + "원";
+        //TotalText.text = cost.ToString() + "원"; //가격텍스트에 표시
     }
 }
